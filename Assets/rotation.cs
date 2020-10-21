44c268b4c661cdfdb2c963c8c9b767483bce1902
FaBLeErroR <72900276+FaBLeErroR@users.noreply.github.com>
2020-10-21 18:08:23 +1000

    Initial commit

---
+++ Assets/rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float X, Y, Z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(X, Y, Z));
+    }
+}
